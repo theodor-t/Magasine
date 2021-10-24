ae688a63decace3b20001c17d40e3c9d9afbd4da
theodorerlax <sampjohn@mail.ru>
2021-10-24 21:05:05 +0300

    create Login Activity and other

--- app/src/main/java/com/example/magasine/RegistrationActivity.java
+++ app/src/main/java/com/example/magasine/RegistrationActivity.java
@@ -2,13 +2,41 @@ package com.example.magasine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    Button signUp;
+    EditText name, email, password;
+    TextView signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        signUp = findViewById(R.id.reg_btn);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
