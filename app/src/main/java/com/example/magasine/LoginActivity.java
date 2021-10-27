7d71e0e48ffc1f281f13922e8e59fc1eb27155fa
theodorerlax <sampjohn@mail.ru>
2021-10-27 18:11:16 +0300

    create LoginUser

--- app/src/main/java/com/example/magasine/LoginActivity.java
+++ app/src/main/java/com/example/magasine/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.magasine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,10 +23,14 @@ public class LoginActivity extends AppCompatActivity {
     EditText email, password;
     TextView signUp;
 
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
 
         signIn = findViewById(R.id.login_btn);
         email = findViewById(R.id.email_login);
@@ -35,7 +47,41 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                loginUser();
             }
         });
+    }
+
+    private void loginUser() {
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this,"Email is Empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this,"Password is Empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(userPassword.length() < 6){
+            Toast.makeText(this,"Password Length must be greater then 6 letter", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Login User
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Login Successfully", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LoginActivity.this,"Error" +task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
