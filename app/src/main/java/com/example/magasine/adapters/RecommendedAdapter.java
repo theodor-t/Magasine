08f37aa818d954eec7b3d6e633956c1f0a4ede07
theodorerlax <sampjohn@mail.ru>
2021-11-02 18:57:23 +0200

    create Recommended Adapter

---
+++ app/src/main/java/com/example/magasine/adapters/RecommendedAdapter.java
@@ -0,0 +1,45 @@
+package com.example.magasine.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.magasine.models.RecommendedModel;
+
+import java.util.List;
+
+public class RecommendedAdapter extends RecyclerView.Adapter<RecommendedAdapter.ViewHolder> {
+
+    Context context;
+    List<RecommendedModel> list;
+
+    public RecommendedAdapter(Context context, List<RecommendedModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
