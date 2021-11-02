3a889fa25ff5114cea671a71dad17bc8178b0636
theodorerlax <sampjohn@mail.ru>
2021-11-02 16:36:25 +0200

    add Cloud Firestore

--- app/src/main/java/com/example/magasine/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/magasine/ui/home/HomeFragment.java
@@ -12,16 +12,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.magasine.R;
+import com.example.magasine.adapters.PopularAdapters;
+import com.example.magasine.models.PopularModel;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView popularRec;
+
     //popular items
+    List<PopularModel> popularModelList;
+    PopularAdapters popularAdapters;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        popularRec = root.findViewById(R.id.pop_rec);
         return root;
     }
 }
