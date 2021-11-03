7c35c83973468b274f3539899aff496a05bd9d3b
theodorerlax <sampjohn@mail.ru>
2021-11-03 17:35:20 +0200

    add FireStore in CategoryFragment

--- app/src/main/java/com/example/magasine/ui/category/CategoryFragment.java
+++ app/src/main/java/com/example/magasine/ui/category/CategoryFragment.java
@@ -4,18 +4,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.magasine.R;
+import com.example.magasine.adapters.NavCategoryAdapter;
+import com.example.magasine.adapters.PopularAdapters;
+import com.example.magasine.models.NavCategoryModel;
+import com.example.magasine.models.PopularModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
+
+    FirebaseFirestore db;
+    RecyclerView recyclerView;
+    List<NavCategoryModel> categoryModelList;
+    NavCategoryAdapter navCategoryAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_category, container, false);
 
+        db = FirebaseFirestore.getInstance();
+        recyclerView = root.findViewById(R.id.cat_rec);
+
+        //Nav Category
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+        categoryModelList = new ArrayList<>();
+        navCategoryAdapter = new NavCategoryAdapter(getActivity(), categoryModelList);
+        recyclerView.setAdapter(navCategoryAdapter);
+
+        db.collection("NavCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                NavCategoryModel navCategoryModel = document.toObject(NavCategoryModel.class);
+                                categoryModelList.add(navCategoryModel);
+                                navCategoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
         return root;
     }
 }
