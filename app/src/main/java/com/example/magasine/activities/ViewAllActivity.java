20e26a26088d72b83738d7ebf9f1523c4b5f1d41
theodorerlax <sampjohn@mail.ru>
2021-11-03 19:54:12 +0200

    create ViewAllActivity

---
+++ app/src/main/java/com/example/magasine/activities/ViewAllActivity.java
@@ -0,0 +1,80 @@
+package com.example.magasine.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.magasine.R;
+import com.example.magasine.adapters.ViewAllAdapter;
+import com.example.magasine.models.ViewAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAllActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+    RecyclerView recyclerView;
+    ViewAllAdapter viewAllAdapter;
+    List<ViewAllModel> viewAllModelList;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        String type = getIntent().getStringExtra("type");
+        recyclerView = findViewById(R.id.view_all_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(this, viewAllModelList);
+        recyclerView.setAdapter(viewAllAdapter);
+
+        //////Getting fruits
+        if (type != null && type.equalsIgnoreCase("fruit")) {
+            firestore.collection("AllProducts").whereEqualTo("type", "fruit").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                    for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()) {
+                        ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                        viewAllModelList.add(viewAllModel);
+                        viewAllAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
+            //////Getting vegetable
+            if (type != null && type.equalsIgnoreCase("vegetable")) {
+                firestore.collection("AllProducts").whereEqualTo("type", "vegetable").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()) {
+                            ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                            viewAllModelList.add(viewAllModel);
+                            viewAllAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+            }
+
+        }
+    }
