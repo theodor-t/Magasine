c570a92584f21ddbf37e8fc7fed9e1ad055a826e
theodorerlax <sampjohn@mail.ru>
2021-11-04 15:06:34 +0200

    Create Activity DetailedActivity and design

---
+++ app/src/main/java/com/example/magasine/activities/DetailedActivity.java
@@ -0,0 +1,16 @@
+package com.example.magasine.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.magasine.R;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+}
