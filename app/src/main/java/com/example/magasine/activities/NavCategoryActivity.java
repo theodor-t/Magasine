e8b64c6eb86b6412cfd496052de284dcf11a105c
theodorerlax <sampjohn@mail.ru>
2021-11-05 15:26:06 +0200

    Nav Category Item (Design part)

---
+++ app/src/main/java/com/example/magasine/activities/NavCategoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.magasine.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.magasine.R;
+
+public class NavCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_category);
+    }
+}
